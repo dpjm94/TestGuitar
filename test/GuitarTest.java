cc66177e85308db4266811e9eb5b8f19d7f00497
Donal Maher <donalmaher@Donals-MacBook-Pro.local>
2016-10-07 22:50:59 +0100

    fix the enums

---
+++ test/GuitarTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by donalmaher on 07/10/2016.
+ */
+public class GuitarTest {
+    @Test
+    public void getSerialNumber() throws Exception {
+
+    }
+
+}
